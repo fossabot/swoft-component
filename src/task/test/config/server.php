071f54017a3d59c3a3dbe11acb22998e60e5f001
huangzhhui <huangzhwork@gmail.com>
2018-04-16 17:58:20 +0800

    add server setting

--- src/task/test/config/server.php
+++ src/task/test/config/server.php
@@ -33,5 +33,13 @@ return [
         'log_file'        => env('LOG_FILE', '@runtime/logs/swoole.log'),
         'task_worker_num' => env('TASK_WORKER_NUM', 1),
         'upload_tmp_dir'  => env('UPLOAD_TMP_DIR', '@runtime/uploadfiles'),
+        'document_root'         => env('DOCUMENT_ROOT', BASE_PATH . '/public'),
+        'enable_static_handler' => env('ENABLE_STATIC_HANDLER', true),
+        'open_http2_protocol'   => env('OPEN_HTTP2_PROTOCOL', false),
+        'ssl_cert_file'         => env('SSL_CERT_FILE', ''),
+        'ssl_key_file'          => env('SSL_KEY_FILE', ''),
+        'task_ipc_mode'         => env('TASK_IPC_MODE', 2),
+        'message_queue_key'     => env('MESSAGE_QUEUE_KEY', 0x70001001),
+        'task_tmpdir'           => env('TASK_TMPDIR', '/tmp'),
     ],
 ];
